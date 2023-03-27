bb2469912e5cbb969d26250dd3c97a41a8296a8e
Hugo Luque <hugooscar731@gmail.com>
2023-03-27 20:01:39 -0300

    segundo commit

---
+++ Definirvariables.java
@@ -0,0 +1,19 @@
+
+package definirvariables;
+
+public class Definirvariables {
+
+      public static void main(String[] args) {
+       
+        char letra;
+        int num;
+        String palabra;
+        double numero;
+        byte a;
+        boolean v;
+        
+    
+    }
+    
+    
+}
---
+++ Guia1_eje1/src/guia1_eje1/Ejercicio1.java
@@ -0,0 +1,28 @@
+/*
+ * Escribir un programa que pida tu nombre, lo guarde en una variable 
+* y lo muestre por pantalla.
+ */
+package guia1_eje1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administracion
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);      
+        String nombre;
+        System.out.println("Ingrese su nombre");
+        nombre = leer.next();
+        System.out.println("su nombre es" + nombre);
+                    
+// TODO code application logic here
+    }
+    
+}
